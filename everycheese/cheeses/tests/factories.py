dea380f54ff050d922960d7ece7d9ae84e8b3025
rgrlinux <rgrlinux@gmail.com>
2020-08-16 18:55:24 -0300

    Implement cheese country of origin

--- everycheese/cheeses/tests/factories.py
+++ everycheese/cheeses/tests/factories.py
@@ -10,6 +10,7 @@ class CheeseFactory(factory.django.DjangoModelFactory):
     name = factory.fuzzy.FuzzyText()
     slug = factory.LazyAttribute(lambda obj: slugify(obj.name))
     firmness = factory.fuzzy.FuzzyChoice([x[0] for x in Cheese.Firmness.choices])
+    country_of_origin = factory.Faker('country_code')
 
     class Meta:
         model = Cheese
